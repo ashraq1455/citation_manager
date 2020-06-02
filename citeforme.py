f87e512cec11abd88c9d347ca00d64cfc170c98a
Mohamed <c4.arham@gmail.com>
2020-06-02 22:12:01 +0500

    - Made some methods, fixed some methods
    Signed-off-by: Mohamed <c4.arham@gmail.com>

--- citeforme.py
+++ citeforme.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, Message, Filters
 import requests
 import datetime
+from intext import get_cite, get_pdf, get_intext
 from bs4 import BeautifulSoup as soup
-
 
 time = datetime.datetime.now()
 
@@ -10,40 +10,7 @@ API_ID =
 API_HASH = ""
 BOT_TOKEN = ""
 
-
 bot = Client("bot", API_ID, API_HASH, BOT_TOKEN)
-
-def pdf(doi):
-    try:
-        URL = f"https://scihub.wikicn.top/{doi}"
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-
-        req = requests.get(URL, headers=headers)
-        html = soup(req.content.decode("utf-8"), "html.parser")
-
-        pdf_link = html.find("iframe", {"id": "pdf"})["src"]
-        print(pdf_link)
-        return pdf_link
-
-    except Exception as e:
-        print(e)
-        return "PDF Not Available!"
-
-
-def cite(doi):
-    
-    headers = {"Accept": "text/x-bibliography", "style": "harward"}
-    r = requests.post(doi, headers=headers)
-
-    full_citation = ""
-    for x in r:
-        full_citation += x.decode("utf-8")
-
-    final = full_citation.replace("doi:", "Available from: https://doi.org/") + f"[Accessed: {time.day} {time.strftime('%B')} {time.year}]."
-
-    print(final)
-    return final
-
 
 @bot.on_message(Filters.command("start", prefixes=['/']))
 async def create_user_profile(bot: bot, msg: Message):
@@ -52,49 +19,45 @@ async def create_user_profile(bot: bot, msg: Message):
     first_name = msg.chat.first_name
     await bot.send_message(chat_id, "Send a DOI number or a DOI link for Harward style citation.")
 
-
 @bot.on_message()
 async def my_function(client, message): 
     chat_id = message.chat.id
     msg = message["text"]
     print(msg)
     if "doi.org" in msg:
+        msg = msg.replace("https://doi.org/", "").replace("doi.org/", "")
         try:
-            
-            citation = cite(msg)
-            if "DOI Not Found" in citation:
+            citation = await get_cite(msg)
+            if not citation:
                 text = "ERROR: DOI Not Found!"
                 print(text)
                 await bot.send_message(chat_id, text)
             else:
-                get_pdf = pdf(msg)
+                get_pdf = await get_pdf(msg)
+                in_text  = await get_intext(msg)
                 await bot.send_message(chat_id, get_pdf)
                 await bot.send_message(chat_id, citation, disable_web_page_preview=True)
-
+                await bot.send_message(chat_id, in_text, disable_web_page_preview=True)
         except Exception as e:
             print(e)
             text = "Citation not available. Make sure your link is correct."
             await bot.send_message(chat_id, text)
     else:
         try:
-            doi_link = f"http://doi.org/{msg}"
-
-            citation = cite(doi_link)
-
-            if "DOI Not Found" in citation:
+            citation = await get_cite(msg)
+            if not citation:
                 text = "ERROR: DOI Not Found!"
                 print(text)
                 await bot.send_message(chat_id, text)
             else:
-                
-                get_pdf = pdf(msg)
+                get_pdf = await get_pdf(msg)
+                in_text  = await get_intext(msg)
                 await bot.send_message(chat_id, get_pdf)
                 await bot.send_message(chat_id, citation, disable_web_page_preview=True)
-        
+                await bot.send_message(chat_id, in_text, disable_web_page_preview=True)
         except Exception as e:
             print(e)
             text = "Citation not available. Make sure your link is correct."
             await bot.send_message(chat_id, text)
-
 bot.run()
 
--- intext.py
+++ intext.py
@@ -1,51 +1,80 @@
 import asyncio
 import aiohttp
+import datetime
 
+from bs4 import BeautifulSoup
+
+p_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 a_headers = {"Accept": "application/rdf+xml;q=0.5, application/vnd.citationstyles.csl+json;q=1.0"}
-c_headers = {"Accept": "text/x-bibliography", "style": "harward"}
+c_headers = {"Accept": "text/x-bibliography; style=harvard-staffordshire-university; locale=en-GB"}
 
-#doi = "10.1108/MAJ-09-2018-2004" 2
-#doi = "10.1108/MAJ-01-2018-1766" 3
-#doi = "10.1108/MAJ-03-2018-1831" 3
-#doi = "10.1108/MAJ-10-2017-1673" 4
-doi = "10.1108/MAJ-08-2014-1065"
+async def get_pdf(doi):
+    try:
+        url = f"https://scihub.wikicn.top/{doi}"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=p_headers) as res:
+                content = await res.text()
+
+        soup = BeautifulSoup(content, "html.parser")
+        pdf_link = soup.find("iframe", {"id": "pdf"})["src"]
+        print(pdf_link)
+        return pdf_link
+
+    except Exception as e:
+        print(e)
+        return None
+
 
 async def get_authors(doi):
+    url = f"https://doi.org/{doi}"
     async with aiohttp.ClientSession() as session:
-        link = f"https://doi.org/{doi}"
-        async with session.get(link, headers=a_headers) as res:
+        async with session.get(url, headers=a_headers) as res:
             response = await res.json()
-    authors = response['author']
+    authors = response['authors']
     return authors
 
-async def get_date(doi):
+async def get_title(doi):
+    url = f"https://doi.org/{doi}"
     async with aiohttp.ClientSession() as session:
-        link = f"https://doi.org/{doi}"
-        async with session.get(link, headers=c_headers) as res:
-            citation = await res.text()
-        date = citation[citation.index("(")+1:citation.index(").")]
-        return date
+        async with session.get(url, headers=a_headers) as res:
+            response = await res.json()
+    journal_title = response['title']
+    return journal_title
 
 
-async def make_intext(doi):
+async def get_cite(doi):
+    time = datetime.datetime.now()
+    url = f"https://doi.org/{doi}"
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=c_headers) as res:
+                citation = await res.text(encoding="utf-8")
+            citation = citation[:citation.index(").")].replace("&", "and") + citation[citation.index(")."):]
+            citation = citation.strip()[:-1] + f" [Accessed On: {time.day} {time.strftime('%B')} {time.year}]."
+            journal_title = await get_title(doi)
+            citation = citation.replace(str(journal_title), f"<i>{journal_title}</i>")
+            return citation
+    except Exception as e:
+        print(e)
+        return None
+
+async def get_intext(doi):
     authors = await get_authors(doi)
+    citation = await get_cite(doi)
+    year = citation[citation.index("(")+1:citation.index(").")]
+    
     num_authors = len(authors)
-    auth_text = "("
+
+    in_text = "("
+    
     if num_authors == 1:
-        auth_text = authors[0]['family']
+        in_text += authors[0]['family']
     elif num_authors == 2:
-        auth_text += f"{authors[0]['family']} & {authors[1]['family']}"
+        in_text += f"{authors[0]['family']} and {authors[1]['family']}"
     elif num_authors == 3:
-        auth_text += f"{authors[0]['family']}, {authors[1]['family']} & {authors[2]['family']}"
-    elif num_authors == 4:
-        auth_text += f"{authors[0]['family']}, {authors[1]['family']}, {authors[2]['family']} & {authors[3]['family']}"
-    elif num_authors > 4:
-        auth_text += f"{authors[0]['family']} et al."
-
-    year = await get_date(doi)
-    auth_text += f", {year})"
-    print(auth_text)
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(make_intext(doi))
+        in_text += f"{authors[0]['family']}, {authors[1]['family']} and {authors[2]['family']}"
+    elif num_authors > 3:
+        in_text += f"{authors[0]['family']} et al."
+    
+    in_text += f", {year})"
+    return in_text
