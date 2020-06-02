7a244161abf2863a40e6d1dee1fafb01feb89958
Mohamed <c4.arham@gmail.com>
2020-06-02 20:42:36 +0500

    - Changed intext to async
    Signed-off-by: Mohamed <c4.arham@gmail.com>

--- intext.py
+++ intext.py
@@ -1,45 +1,51 @@
-import requests
+import asyncio
+import aiohttp
+
+a_headers = {"Accept": "application/rdf+xml;q=0.5, application/vnd.citationstyles.csl+json;q=1.0"}
+c_headers = {"Accept": "text/x-bibliography", "style": "harward"}
+
+#doi = "10.1108/MAJ-09-2018-2004" 2
+#doi = "10.1108/MAJ-01-2018-1766" 3
+#doi = "10.1108/MAJ-03-2018-1831" 3
+#doi = "10.1108/MAJ-10-2017-1673" 4
+doi = "10.1108/MAJ-08-2014-1065"
+
+async def get_authors(doi):
+    async with aiohttp.ClientSession() as session:
+        link = f"https://doi.org/{doi}"
+        async with session.get(link, headers=a_headers) as res:
+            response = await res.json()
+    authors = response['author']
+    return authors
+
+async def get_date(doi):
+    async with aiohttp.ClientSession() as session:
+        link = f"https://doi.org/{doi}"
+        async with session.get(link, headers=c_headers) as res:
+            citation = await res.text()
+        date = citation[citation.index("(")+1:citation.index(").")]
+        return date
 
 
+async def make_intext(doi):
+    authors = await get_authors(doi)
+    num_authors = len(authors)
+    auth_text = "("
+    if num_authors == 1:
+        auth_text = authors[0]['family']
+    elif num_authors == 2:
+        auth_text += f"{authors[0]['family']} & {authors[1]['family']}"
+    elif num_authors == 3:
+        auth_text += f"{authors[0]['family']}, {authors[1]['family']} & {authors[2]['family']}"
+    elif num_authors == 4:
+        auth_text += f"{authors[0]['family']}, {authors[1]['family']}, {authors[2]['family']} & {authors[3]['family']}"
+    elif num_authors > 4:
+        auth_text += f"{authors[0]['family']} et al."
 
-#curl -LH "Accept: application/rdf+xml;q=0.5, application/vnd.citationstyles.csl+json;q=1.0" https://doi.org/10.1126/science.169.3946.635
-
-
-doi = "https://doi.org/10.1108/MAJ-09-2018-2004"
-
-
-headers = {"Accept": "application/rdf+xml;q=0.5, application/vnd.citationstyles.csl+json;q=1.0"}
-
-
-r = requests.post(doi, headers=headers).json()
-#print(r)
-authors = r["author"]
-
-print(authors)
-InText = ""
-
-
-
-citation = "Haapamäki, E., & Sihvonen, J. (2019). Cybersecurity in accounting research. Managerial Auditing Journal, 34(7), 808–834. Available from: https://doi.org/10.1108/maj-09-2018-2004 [Accessed: 2 June 2020]."
+    year = await get_date(doi)
+    auth_text += f", {year})"
+    print(auth_text)
 
 
-published_date = citation[citation.index("(")+1:citation.index(").")]
-
-
-if len(authors) > 3:
-    InText = authors[0]["family"] + " et al.," + published_date
-    print(InText)
-
-elif len(authors) == 3:
-    InText = f"({authors["family"][0].title()},  {authors["family"][1].title()} and {authors["family"][2].title()}, {published_date})"
-    print(InText)
-
-elif len(authors) == 2:
-    InText = f"({authors["family"][0].title()} and {authors["family"][1].title()}, {published_date})"
-    print(InText)
-
-elif len(authors) == 1:
-    InText = f"({authors["family"][0].title()}, {published_date})"
-    print(InText)
-
-
+loop = asyncio.get_event_loop()
+loop.run_until_complete(make_intext(doi))
